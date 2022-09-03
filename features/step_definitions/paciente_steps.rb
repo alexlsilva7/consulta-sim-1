89d8663e663eb3cc22e00586e856d3a2f0d5a28c
Alex <alex.lsilva154@gmail.com>
2022-09-03 17:53:07 -0300

    adicionado cenario remover paciente da feature paciente

--- features/step_definitions/paciente_steps.rb
+++ features/step_definitions/paciente_steps.rb
@@ -22,3 +22,33 @@ Then('eu vejo uma mensagem que informa que o paciente foi criado com sucesso') d
   page.has_content?('Paciente was successfully created.')
 end
 
+Given('estou na pagina de pacientes') do
+  visit '/pacientes'
+end
+
+Given('O paciente de nome: {string}, data_nascimeto: {string}, cpf: {string}, email: {string}, cidade: {string},logradouro: {string},complemento: {string},bairro: {string},cep: {string} existe') do |nome, data_nascimeto, cpf, email, cidade, logradouro, complemento, bairro, cep|
+  visit '/pacientes/new'
+  fill_in 'paciente[nome]', :with => nome
+  fill_in 'paciente[data_nascimento]', :with => data_nascimeto
+  fill_in 'paciente[cpf]', :with => cpf
+  fill_in 'paciente[email]', :with => email
+  fill_in 'paciente[endereco_attributes][cep]', :with => cep
+  fill_in 'paciente[endereco_attributes][cidade]', :with => cidade
+  fill_in 'paciente[endereco_attributes][bairro]', :with => bairro
+  fill_in 'paciente[endereco_attributes][logradouro]', :with => logradouro
+  fill_in 'paciente[endereco_attributes][complemento]', :with => complemento
+  click_button 'Create Paciente'
+  visit '/pacientes'
+end
+
+Given('estou na pagina do paciente com cpf {string}') do |cpf|
+  visit "/pacientes/#{Paciente.find_by_cpf(cpf).id}"
+end
+
+When('eu clico em remover o paciente') do
+  click_button 'Destroy this paciente'
+end
+
+Then('eu vejo uma mensagem que informa que o paciente foi apagado com sucesso') do
+  page.has_content?('Paciente was successfully destroyed.')
+end
