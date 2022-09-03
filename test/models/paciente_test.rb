ba565da36070ae9d8d0db946961619f946397700
Alex <alex.lsilva154@gmail.com>
2022-09-03 17:24:37 -0300

    adicionado 2 testes em paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "deve salvar paciente corretamente" do
+    endereco = Endereco.new(cidade: 'Recife', logradouro: 'rua rosa', complemento: 'casa', bairro: 'Boa Viagem', cep: '55555-555')
+    paciente = Paciente.new(nome: 'Paciente Teste', data_nascimento: '10-10-1980', cpf: '123797525352', email: 'pacienteteste@gmail.com')
+    paciente.endereco = endereco
+
+    assert paciente.save
+  end
+
+  test "não deve salvar Paciente com campo data_nascimeto vazio" do
+    endereco = Endereco.new(cidade: 'Recife', logradouro: 'rua rosa', complemento: 'casa', bairro: 'Boa Viagem', cep: '55555-555')
+    paciente = Paciente.new(nome: 'Paciente Teste', data_nascimento: '', cpf: '123797525352', email: 'pacienteteste@gmail.com')
+    paciente.endereco = endereco
+
+    assert_not paciente.save
+  end
 end
